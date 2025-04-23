b9723a1983ab44952e485a14b419eaf337e428b2
jusspian <luisfjuspian@gmail.com>
2025-04-23 19:39:51 +0000

    generador excusas refactorizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  function excuse(arr){
+    let random = Math.floor(Math.random()*arr.length);
+    return random;
+  };
+
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  let quien =  who[excuse(who)];
+  let cosa = action[excuse(action)];
+  let que = what[excuse(what)];
+  let cuando = when[excuse(when)];
+
+  let excu = quien + " " + cosa + " " + que + " "+ cuando;
+
+  document.getElementById("excuse").innerHTML = excu;
+}
